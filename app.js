84a826745845cc013aa49c2743643ef022d157e7
Amal Medlege <amal@Amals-MacBook-Pro-2.local>
2022-10-22 21:41:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Variables
+const state = {
+    currentTool: "",
+    inventory: [],
+    tools: [],
+    currentMaterial: "", 
+
+}
+
+//functions we will need
+
+// create 2d grid for the game
+function landScapeMaker (material, ){
+
+}
+
+// basic world builder with set positions
+function landMaker() {
+landScapeMaker()
+landScapeMaker()
+landScapeMaker()
+landScapeMaker()
+
+}
+
+
+
+
